126fae9d2951e0be60a7ec2b5b584f48fc5e4536
sinoritamiamore <sinoritamiamore@gmail.com>
2025-11-11 23:12:52 -0800

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,18 @@
+import Fastify from 'fastify';
+import cors from 'fastify-cors';
+import path from 'path';
+import fastifyStatic from 'fastify-static';
+import infoRoutes from './routes/info';
+import calendarRoutes from './routes/calendar';
+import resourcesRoutes from './routes/resources';
+import chatRoutes from './routes/chat';
+import { PrismaClient } from '@prisma/client';
 
+const prisma = new PrismaClient();
+const server = Fastify({ logger: true });
+
+server.register(cors, { origin: true });
+server.register(fastifyStatic, {
+root: path.join(__dirname, '..', 'public'),
+prefix: '/resources/',
+});
